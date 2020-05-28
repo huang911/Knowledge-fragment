e3f3220988860f50029b4c9c93e7a442685a3a40
huangqi <huangqi_h@qq.com>
2020-05-28 10:04:27 +0800

    删除文件

--- daily/跨域/cross-domain/jsonp/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express')
-let app = express()
-
-app.get('/say', function(req, res) {
-  let { wd, cb } = req.query;
-  console.log(wd);
-  res.end(`${cb}('test')`);
-})
-app.listen(3000)
